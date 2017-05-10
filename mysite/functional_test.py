a26068860bc38503c9a600fd6fc0bd345af42bab
a5720128a <a5720128a@gmail.com>
2017-05-10 16:56:35 +0700

    unit_test and functional_test

--- mysite/functional_test.py
+++ mysite/functional_test.py
@@ -19,30 +19,58 @@ class NewVisitorTest(unittest.TestCase):
         
         # He notices the page title "Index"
         self.assertIn('Index', self.browser.title)
-        self.fail('Finish the test!')
-        
+
         # He clicked play button
+        link = self.browser.find_element_by_tag_name("play")
+        link.click()
 	
 	# He notices the page title "MBTI Test"
-	self.assertIn('Index', self.browser.title)
+	self.assertIn('MBTI Test', self.browser.title)
 	# He answer the test
         # He choosed all green choices
-
+        for i in range(1,41):
+            browser.find_elements_by_css("input[type='radio'][id="+i+"][value>0]").click
+        
         # He type his name "Chris" and his facebook "Chris Deedee"
-
+        inputbox = self.browser.find_element_by.name('addPlayer')
+        inputbox.send_keys('Chris')
+        inputbox2 = self.browser.find_element_by.name('add_Facebook')
+        inputbox.send_keys('Chris Deedee')
+	link = self.browser.find_elements_by_class_name("button button1")
+        link.click()
+        
         # He notices the page title "Result"
+        self.assertIn('Result', self.browser.title)
         # He found he is "ESTJ"
         # He clicked info button
+        html_source = browser.page_source
+        if "ESTJ" in html_source:
+            link = self.browser.find_element_by_tag_name("info")
+            link.click()
+        else:
+            self.fail('fail')
 
         # The page linked to his personality
+        self.assertIn('ESTJ', self.browser.title)
 
         # He found his facebook below
         # He clicked "ESFJ" for see Get along well with personality
+        if "Chris Deedee" in html_source:
+            link = self.browser.find_element_by_tag_name("ESFJ")
+            link.click()
+        else:
+            self.fail('fail')
+        
 
         # He visits index agian and clicked about
+        link = self.browser.find_element_by_tag_name("home")
+        link.click()
         # He notices the page title "About"
+        link = self.browser.find_element_by_tag_name("about")
+        link.click()
 
         # He goes back to sleep
+	self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- mysite/quiz/tests.py
+++ mysite/quiz/tests.py
@@ -1,3 +1,18 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+from quiz.models import Question, Choice, Player, Personality, Compatibility
 
-# Create your tests here.
+
+class ModelTest(TestCase):
+
+    def create_whatever(self, title="INFJ", body="yes, this is only a test", body2="test too"):
+        return Player.objects.create(player_text = body, personaltiy = title, facebook = body2, pub_date=timezone.now())
+
+    def test_whatever_creation(self):
+        w = self.create_Player()
+        self.assertTrue(isinstance(w, Player))
+        self.assertEqual(w.__unicode__(), w.title)
+
